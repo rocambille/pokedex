ec165e78d09a5ed32d255eb709135e8f8a8bc341
romaing <rocambille@gmail.com>
2022-06-28 14:16:09 +0200

    step 2: conditional rendering

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,24 @@
-const pokemon = {
-  name: "bulbasaur",
-  imgSrc:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-};
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function PokemonCard() {
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
