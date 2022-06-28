f9d4331d20f0769f53c7f4cd91c8432cca161d4c
romaing <rocambille@gmail.com>
2022-06-28 14:07:56 +0200

    step 1: displaying data

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,14 @@
+const pokemon = {
+  name: "bulbasaur",
+  imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+};
+
 function PokemonCard() {
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      />
-      <figcaption>bulbasaur</figcaption>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
