99488fc4b87ac9aef32cde1b73569caa6da81936
romaing <rocambille@gmail.com>
2022-06-28 15:53:20 +0200

    step 1: mapped pokemons in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,17 @@ import PropTypes from "prop-types";
 import PokemonType from "../prop-types/PokemonType";
 
 function NavBar({ setPokemonIndex, pokemonList }) {
-  const previous = () =>
-    setPokemonIndex(
-      (previous) => (previous - 1 + pokemonList.length) % pokemonList.length
-    );
-
-  const next = () =>
-    setPokemonIndex((previous) => (previous + 1) % pokemonList.length);
-
   return (
     <div>
-      <button type="button" onClick={previous}>
-        previous
-      </button>
-      <button type="button" onClick={next}>
-        next
-      </button>
+      {pokemonList.map((pokemon, index) => (
+        <button
+          type="button"
+          onClick={() => setPokemonIndex(index)}
+          key={pokemon.name}
+        >
+          {pokemon.name}
+        </button>
+      ))}
     </div>
   );
 }
