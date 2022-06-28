e5ed7c387149706a106d17dde29c9caf99678511
romaing <rocambille@gmail.com>
2022-06-28 13:51:11 +0200

    step 1: static jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ function App() {
             count is: {count}
           </button>
         </p>
+        <figure>
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            alt="bulbasaur"
+          />
+          <figcaption>bulbasaur</figcaption>
+        </figure>
       </header>
     </div>
   );
